265e94df1fe01a708d6b653babc13ced9e431ea2
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-10-20 11:12:26 -0300

    Set, telas, facade

--- src/br/com/argus/app/App.java
+++ src/br/com/argus/app/App.java
@@ -4,20 +4,12 @@
  * and open the template in the editor.
  */
 package br.com.argus.app;
-
-import br.com.argus.exceptions.FXMLExcepton;
-import br.com.argus.facade.Facade;
-import br.com.argus.facade.IFacade;
-import br.com.argus.model.Usuario;
-import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Modality;
-import javafx.stage.WindowEvent;
+import javafx.scene.layout.Pane;
+
 
 /**
  *
@@ -25,77 +17,69 @@ import javafx.stage.WindowEvent;
  */
 public class App extends Application {
     
-    public static final String Tela_Login = "/br/com/argus/view/Login.fxml";
-    public static final String Tela_Principal = "/br/com/argus/view/Dashboard.fxml";
-    public static final String Tela_Cadastro_Usuario = "/br/com/argus/view/Cadastrar_Usuario.fxml";
-    public static final String Tela_Senha = "/br/com/argus/view/Reset_Senha.fxml";
-    
-    
-    public static Stage STAGE = new Stage();
-    public static Stage STAGE_DASHBOARD = new Stage();
+    private static Scene sceneLogin, sceneHome;
+    private static Stage stage;
+//    public final static String Tela_Login = "login";
+//    public final static String Tela_Home = "home";
     
     @Override
-    public void start(Stage primayStage) throws FXMLExcepton, IOException{
-        STAGE.initOwner(STAGE_DASHBOARD);
-        STAGE.initModality(Modality.WINDOW_MODAL);
+    public void start(Stage primayStage){
+        try {
+            
+           Pane telaLogin = FXMLLoader.load(getClass().getResource("/br/com/argus/view/Login.fxml"));
+           
+           sceneLogin = new Scene(telaLogin);
         
-        stageLogin().show();
-
+            primayStage.setScene(sceneLogin);
+            primayStage.centerOnScreen();
+            primayStage.show();
+            setStage(stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }    
     }
     
-    public static Stage stageLogin() throws IOException{
-        STAGE.setScene(new Scene(FXMLLoader.load(App.class.getResource(Tela_Login))));
-        STAGE.setResizable(false);
-        STAGE.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent event) { STAGE.close();
-                
-            }
-        });
-        return STAGE;
+    public void startA(Stage primayStage){
+        try {
+            
+           Pane telaHome = FXMLLoader.load(getClass().getResource("/br/com/argus/view/Dashboard.fxml"));
+           
+           sceneHome = new Scene(telaHome);
+        
+            primayStage.setScene(sceneHome);
+            primayStage.centerOnScreen();
+            primayStage.show();
+            setStage(stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }    
     }
     
-    public static Stage stageDashboard() throws IOException{
-        STAGE_DASHBOARD.setScene(new Scene(FXMLLoader.load(App.class.getResource(Tela_Principal))));
-        STAGE.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent event) { System.exit(0);
-                
-            }
-        });
-        return STAGE_DASHBOARD;
+    public static void setStage(Stage Sstage){
+         stage = Sstage;
     }
     
-       public static Stage stageGenerica(String caminho) throws IOException{
-        STAGE.setScene(new Scene(FXMLLoader.load(App.class.getResource(caminho))));
-        STAGE.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent event) { STAGE.close();
-                
-            }
-        });
-        return STAGE;
+    public static Stage getStage (){
+        return stage;
     }
-    
-    
-    
-    
-
+//    
+//    public static void MudarTela (String tela){
+//        if(tela.equals("home")){
+//            stage.setScene(sceneHome);
+//            stage.setResizable(false);
+//        }else{
+//            stage.setScene(sceneLogin);
+//            stage.setResizable(false);
+//          
+//        }
+//    }
+  
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-        IFacade facade = Facade.getInstance();
-        
-//        Usuario usuario = new Usuario();
-//        
-//        usuario.setLogin("admin");
-//        usuario.setNome("Administrador");
-//        usuario.setSenha("admin");
-//        
-//        try {
-//            facade.inserirOuAtualizar(usuario);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+//        IFacade facade = Facade.getInstance();
         
         launch(args);
        

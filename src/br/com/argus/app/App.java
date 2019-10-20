4d1d37aa6beddd8616b0cb8d26c4f5112475e573
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-10-20 20:57:55 -0300

    Alterações nos panes, stages e FXMLLoader

--- src/br/com/argus/app/App.java
+++ src/br/com/argus/app/App.java
@@ -10,12 +10,15 @@ import br.com.argus.facade.Facade;
 import br.com.argus.facade.IFacade;
 import br.com.argus.model.SuperUsuario;
 import br.com.argus.model.Usuario;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
-
+import javafx.stage.Modality;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 
 /**
  *
@@ -23,10 +26,28 @@ import javafx.scene.layout.Pane;
  */
 public class App extends Application {
     
-    private static Scene sceneLogin, sceneHome;
-    private static Stage stage;
+//    private static Scene sceneLogin, sceneHome;
+//    private static Stage stage;
 //    public final static String Tela_Login = "login";
 //    public final static String Tela_Home = "home";
+    
+    public static final String LOGIN = "/br/com/argus/view/Login.fxml";
+    public static final String HOME = "/br/com/argus/view/Dashboard.fxml";
+    public static final String RESET_SENHA = "/br/com/argus/view/Resetar_Senha.fxml" ;
+    public static final String ALTERA_SENHA = "/br/com/argus/view/Alterar_senha.fxml" ;
+    public static final String CADASTRO_ALUNO ="/br/com/argus/view/Cadastrar_Aluno.fxml" ;
+    public static final String CADASTRO_DISCIPLINA = "/br/com/argus/view/Cadastrar_Disciplina.fxml" ;
+    public static final String CADASTRO_PROFESSOR = "/br/com/argus/view/Cadastrar_Professor.fxml" ;
+    public static final String CADASTRO_TURMA = "/br/com/argus/view/Cadastrar_Turma.fxml" ;
+    public static final String CADASTRO_USUARIO = "/br/com/argus/view/Cadastrar_Usuario.fxml" ;
+    public static final String VER_ALUNOS = "/br/com/argus/view/Ver_Alunos.fxml" ;
+    public static final String VER_DISCIPLINAS ="/br/com/argus/view/Ver_Disciplinas.fxml" ;
+    public static final String VER_PROFESSORES = "/br/com/argus/view/Ver_Professores.fxml" ;
+    public static final String VER_TURMA = "/br/com/argus/view/Ver_Turma.fxml" ;
+    public static final String VER_USUARIO = "/br/com/argus/view/Ver_Usuarios.fxml" ;
+    
+    public static  Stage STAGE = new Stage();
+    public static  Stage STAGE_PRINCIPAL = new Stage();
     
     public static void main(String[] args) throws BussinesException{
         
@@ -44,46 +65,99 @@ public class App extends Application {
         launch(args);
        
     }
-    @Override
-    public void start(Stage primayStage){
-        try {
-            
-           Pane telaLogin = FXMLLoader.load(getClass().getResource("/br/com/argus/view/Login.fxml"));
-           
-           sceneLogin = new Scene(telaLogin);
+    
+       @Override
+    public void start(Stage primaryStage) throws Exception {
         
-            primayStage.setScene(sceneLogin);
-            primayStage.centerOnScreen();
-            primayStage.show();
-            setStage(stage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }    
+      STAGE.initOwner(STAGE_PRINCIPAL);
+      STAGE.initModality(Modality.WINDOW_MODAL);
+      
+      stageLogin().show();
+      
     }
     
-    public void startA(Stage primayStage){
-        try {
-            
-           Pane telaHome = FXMLLoader.load(getClass().getResource("/br/com/argus/view/Dashboard.fxml"));
-           
-           sceneHome = new Scene(telaHome);
-        
-            primayStage.setScene(sceneHome);
-            primayStage.centerOnScreen();
-            primayStage.show();
-            setStage(stage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }    
-    }
     
-    public static void setStage(Stage Sstage){
-         stage = Sstage;
+	public static Stage stagePrincipal() throws IOException {
+		STAGE_PRINCIPAL.setScene(new Scene(
+				FXMLLoader.load(App.class.getResource (HOME))));
+		STAGE.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			public void handle(WindowEvent event) {
+			    System.exit(0);}});
+		return  STAGE_PRINCIPAL;
+	}
+
+
+    public static  Stage stageLogin() throws IOException {
+        STAGE.setScene(new Scene(
+                FXMLLoader.load(App.class.getResource(LOGIN))));
+        STAGE.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent event) { STAGE.close(); }});
+        return STAGE;
     }
+
+
+	public static Stage genericaStage(String caminho) throws IOException {
+		STAGE.setScene(new Scene(
+				FXMLLoader.load(App.class.getResource (caminho))));
+		STAGE.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			public void handle(WindowEvent event) { STAGE.close(); }});
+		return STAGE;
+	}
     
-    public static Stage getStage (){
-        return stage;
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    @Override
+//    public void start(Stage primayStage){
+//        try {
+//            
+//           Pane telaLogin = FXMLLoader.load(getClass().getResource("/br/com/argus/view/Login.fxml"));
+//           
+//           sceneLogin = new Scene(telaLogin);
+//        
+//            primayStage.setScene(sceneLogin);
+//            primayStage.centerOnScreen();
+//            primayStage.show();
+//            setStage(stage);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }    
+//    }
+
+ 
+    
+//    public void startA(Stage primayStage){
+//        try {
+//            
+//           Pane telaHome = FXMLLoader.load(getClass().getResource("/br/com/argus/view/Dashboard.fxml"));
+//           
+//           sceneHome = new Scene(telaHome);
+//        
+//            primayStage.setScene(sceneHome);
+//            primayStage.centerOnScreen();
+//            primayStage.show();
+//            setStage(stage);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }    
+//    }
+    
+//    public static void setStage(Stage Sstage){
+//         stage = Sstage;
+//    }
+//    
+//    public static Stage getStage (){
+//        return stage;
+//    }
 //    
 //    public static void MudarTela (String tela){
 //        if(tela.equals("home")){

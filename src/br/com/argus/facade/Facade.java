9acd125a03d84244fb04a57db8019f35c72e10cd
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-10-20 13:38:46 -0300

    Implementação do método FACADE, todas as classes do model e seus respectivos métodos

--- src/br/com/argus/facade/Facade.java
+++ src/br/com/argus/facade/Facade.java
@@ -7,21 +7,42 @@ package br.com.argus.facade;
 
 import br.com.argus.business.AlunoBusiness;
 import br.com.argus.business.ContatoBusiness;
+import br.com.argus.business.CoordenadorBusinees;
+import br.com.argus.business.DiretorBusiness;
+import br.com.argus.business.DisciplinaBusiness;
 import br.com.argus.business.EnderecoBusiness;
 import br.com.argus.business.IAlunoBusiness;
 import br.com.argus.business.IContatoBusiness;
+import br.com.argus.business.ICoordenadorBusiness;
+import br.com.argus.business.IDiretorBusiness;
+import br.com.argus.business.IDisciplinaBusiness;
 import br.com.argus.business.IEnderecoBusiness;
+import br.com.argus.business.IProfessorBusiness;
+import br.com.argus.business.IRes_FinBusiness;
+import br.com.argus.business.ISecretarioBusiness;
+import br.com.argus.business.ISuperUsuarioBusiness;
+import br.com.argus.business.ITurmaBisiness;
 import br.com.argus.business.IUsuarioBusiness;
+import br.com.argus.business.ProfessorBusiness;
+import br.com.argus.business.Res_FinBusiness;
+import br.com.argus.business.SecretarioBusiness;
+import br.com.argus.business.SuperUsuarioBusiness;
+import br.com.argus.business.TurmaBisiness;
 import br.com.argus.business.UsuarioBusiness;
 import br.com.argus.exceptions.BussinesException;
-import br.com.argus.exceptions.DAOException;
 import br.com.argus.model.Aluno;
 import br.com.argus.model.Contato;
 import br.com.argus.model.Endereco;
 import br.com.argus.model.Usuario;
+import br.com.argus.model.Coordenador;
+import br.com.argus.model.Diretor;
+import br.com.argus.model.Disciplina;
+import br.com.argus.model.Professor;
+import br.com.argus.model.Resp_Financeiro;
+import br.com.argus.model.SuperUsuario;
+import br.com.argus.model.Turma;
+import br.com.argus.model.Secretario;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -43,17 +64,33 @@ public class Facade implements IFacade{
     private IEnderecoBusiness enderecoBusiness;
     private IContatoBusiness contatoBusiness;
     private IAlunoBusiness alunoBusiness;
+    private ICoordenadorBusiness coordenadorBusiness;
+    private IDiretorBusiness diretorBusiness;
+    private IDisciplinaBusiness disciplinaBusiness;
+    private IProfessorBusiness professorBusiness;
+    private IRes_FinBusiness res_FinBusiness;
+    private ISuperUsuarioBusiness superUsuarioBusiness;
+    private ITurmaBisiness turmaBisiness;
+    private ISecretarioBusiness secretarioBusiness;
     
     private Facade(){
         usuarioBusiness = new UsuarioBusiness();
         enderecoBusiness = new EnderecoBusiness();
         contatoBusiness = new ContatoBusiness();
         alunoBusiness = new AlunoBusiness();
+        coordenadorBusiness = new CoordenadorBusinees();
+        diretorBusiness = new DiretorBusiness();
+        disciplinaBusiness = new DisciplinaBusiness();
+        professorBusiness = new ProfessorBusiness();
+        res_FinBusiness = new Res_FinBusiness();
+        superUsuarioBusiness = new SuperUsuarioBusiness();
+        turmaBisiness = new TurmaBisiness();
+        secretarioBusiness = new SecretarioBusiness();
     }
 
 
 
-    //Usuario
+    //  Usuario
     @Override
     public Usuario buscarLoginUsuario(String login, String senha) throws BussinesException {
         return usuarioBusiness.buscarLoginUsuario(login, senha);
@@ -80,7 +117,7 @@ public class Facade implements IFacade{
     }
     
     
-    // Endereco
+    //  Endereco
     @Override
     public void inserirOuAtualizarEndereco(Endereco endereco) throws BussinesException {
         enderecoBusiness.criarOuAtualizar(endereco);
@@ -103,7 +140,7 @@ public class Facade implements IFacade{
     }
     
     
-    // Contato
+    //  Contato
     @Override
     public void inserirOuAtualizarContato(Contato contato) throws BussinesException {
         contatoBusiness.criarOuAtualizar(contato);
@@ -126,7 +163,7 @@ public class Facade implements IFacade{
     }
 
     
-    //Aluno
+    //  Aluno
     @Override
     public void inserirOuAtualizarAluno(Aluno aluno) throws BussinesException {
         alunoBusiness.criarOuAtualizar(aluno);
@@ -147,6 +184,195 @@ public class Facade implements IFacade{
     public List<Aluno> buscarTodosAlunos(Aluno aluno) throws BussinesException {
         return alunoBusiness.buscarTodos();
     }
+    
+    
+    //  Coordenador
+    @Override
+    public void inserirOuAtualizarCoordenador(Coordenador coordenador) throws BussinesException {
+        coordenadorBusiness.criarOuAtualizar(coordenador);
+    }
+    @Override
+    public void desabilitarCoordenador(Coordenador coordenador) throws BussinesException {
+        coordenadorBusiness.desabilitar(coordenador);
+    }
+    @Override
+    public void deletarCoordenador(Coordenador coordenador) throws BussinesException {
+       coordenadorBusiness.deletar(coordenador);
+    }
+    @Override
+    public void buscarCoordenador(Coordenador coordenador) throws BussinesException {
+        coordenadorBusiness.buscar(Coordenador.class, coordenador.getId());
+    }
+    @Override
+    public List<Coordenador> buscarTodosCoordenadores(Coordenador coordenador) throws BussinesException {
+       return coordenadorBusiness.buscarTodos();
+    }
+
+    
+    //  Diretor
+    @Override
+    public void inserirOuAtualizarDiretor(Diretor diretor) throws BussinesException {
+        diretorBusiness.criarOuAtualizar(diretor);
+    }
+    @Override
+    public void desabilitarDiretor(Diretor diretor) throws BussinesException {
+        diretorBusiness.desabilitar(diretor);
+    }
+    @Override
+    public void deletarDiretor(Diretor diretor) throws BussinesException {
+        diretorBusiness.deletar(diretor);
+    }
+    @Override
+    public void buscarDiretor(Diretor diretor) throws BussinesException {
+        diretorBusiness.buscar(Diretor.class, diretor.getId());
+    }
+    @Override
+    public List<Diretor> buscarTodosDiretores(Diretor diretor) throws BussinesException {
+        return diretorBusiness.buscarTodos();
+    }
+
+    
+    //  Disciplina
+    @Override
+    public void inserirOuAtualizarDisciplina(Disciplina disciplina) throws BussinesException {
+        disciplinaBusiness.criarOuAtualizar(disciplina);
+    }
+    @Override
+    public void desabilitarDisciplina(Disciplina disciplina) throws BussinesException {
+        disciplinaBusiness.desabilitar(disciplina);
+    }
+    @Override
+    public void deletarDisciplina(Disciplina disciplina) throws BussinesException {
+        disciplinaBusiness.deletar(disciplina);
+    }
+    @Override
+    public void buscarDisciplina(Disciplina disciplina) throws BussinesException {
+        disciplinaBusiness.buscar(Disciplina.class, disciplina.getId());
+    }
+    @Override
+    public List<Disciplina> buscarTodosDisciplinas(Disciplina disciplina) throws BussinesException {
+        return disciplinaBusiness.buscarTodos();
+    }
+
+    
+    //  Professor
+    @Override
+    public void inserirOuAtualizarProfessor(Professor professor) throws BussinesException {
+        professorBusiness.criarOuAtualizar(professor);
+    }
+    @Override
+    public void desabilitarProfessor(Professor professor) throws BussinesException {
+        professorBusiness.desabilitar(professor);
+    }
+    @Override
+    public void deletarProfessor(Professor professor) throws BussinesException {
+        professorBusiness.deletar(professor);
+    }
+    @Override
+    public void buscarProfessor(Professor professor) throws BussinesException {
+        professorBusiness.buscar(Professor.class, professor.getId());
+    }
+    @Override
+    public List<Professor> buscarTodosProfessores(Professor professor) throws BussinesException {
+        return professorBusiness.buscarTodos();
+    }
+    
+    
+    //  Responsavel Financeiro
+    @Override
+    public void inserirOuAtualizarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException {
+       res_FinBusiness.criarOuAtualizar(resp_Financeiro);
+    }
+    @Override
+    public void desabilitarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException {
+        res_FinBusiness.desabilitar(resp_Financeiro);
+    }
+    @Override
+    public void deletarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException {
+        res_FinBusiness.deletar(resp_Financeiro);
+    }
+    @Override
+    public void buscarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException {
+        res_FinBusiness.buscar(Resp_Financeiro.class, resp_Financeiro.getId());
+    }
+    @Override
+    public List<Resp_Financeiro> buscarTodosResp_FinS(Resp_Financeiro resp_Financeiro) throws BussinesException {
+        return res_FinBusiness.buscarTodos();
+    }
+
+    
+    //  Secretario
+    @Override
+    public void inserirOuAtualizarSecretario(Secretario secretario) throws BussinesException {
+        secretarioBusiness.criarOuAtualizar(secretario);
+    }
+    @Override
+    public void desabilitarSecretario(Secretario secretario) throws BussinesException {
+        secretarioBusiness.desabilitar(secretario);
+    }
+    @Override
+    public void deletarSecretario(Secretario secretario) throws BussinesException {
+            secretarioBusiness.deletar(secretario);
+    }
+    @Override
+    public void buscarSecretario(Secretario secretario) throws BussinesException {
+        secretarioBusiness.buscar(Secretario.class, secretario.getId());
+    }
+    @Override
+    public List<Secretario> buscarTodosSecretario(Secretario secretario) throws BussinesException {
+        return secretarioBusiness.buscarTodos();
+    }
+
+    
+    //  SuperUsuario
+    @Override
+    public void inserirOuAtualizarSuperUsuario(SuperUsuario superUsuario) throws BussinesException {
+        superUsuarioBusiness.criarOuAtualizar(superUsuario);
+    }
+    @Override
+    public void desabilitarSuperUsuario(SuperUsuario superUsuario) throws BussinesException {
+        superUsuarioBusiness.desabilitar(superUsuario);
+    }
+    @Override
+    public void deletarSuperUsuario(SuperUsuario superUsuario) throws BussinesException {
+      superUsuarioBusiness.deletar(superUsuario);
+    }
+    @Override
+    public void buscarSuperUsuario(SuperUsuario superUsuario) throws BussinesException {
+        superUsuarioBusiness.buscar(SuperUsuario.class, superUsuario.getId());
+    }
+
+    @Override
+    public List<SuperUsuario> buscarTodosSuperUsuarioS(SuperUsuario superUsuario) throws BussinesException {
+        return superUsuarioBusiness.buscarTodos();
+    }
+
+    
+    //  Turma
+    @Override
+    public void inserirOuAtualizarTurma(Turma turma) throws BussinesException {
+        turmaBisiness.criarOuAtualizar(turma);
+    }
+    @Override
+    public void desabilitarTurma(Turma turma) throws BussinesException {
+        turmaBisiness.desabilitar(turma);
+    }
+    @Override
+    public void deletarCTurma(Turma turma) throws BussinesException {
+        turmaBisiness.deletar(turma);
+    }
+    @Override
+    public void buscarTurma(Turma turma) throws BussinesException {
+        turmaBisiness.buscar(Turma.class, turma.getId());
+    }
+
+    @Override
+    public List<Turma> buscarTodosTurma(Turma turma) throws BussinesException {
+        return turmaBisiness.buscarTodos();
+    }
+    
+    
+
     
     
     
--- src/br/com/argus/facade/IFacade.java
+++ src/br/com/argus/facade/IFacade.java
@@ -11,6 +11,14 @@ import br.com.argus.model.Aluno;
 import br.com.argus.model.Contato;
 import br.com.argus.model.Endereco;
 import br.com.argus.model.Usuario;
+import br.com.argus.model.Coordenador;
+import br.com.argus.model.Diretor;
+import br.com.argus.model.Disciplina;
+import br.com.argus.model.Professor;
+import br.com.argus.model.Resp_Financeiro;
+import br.com.argus.model.SuperUsuario;
+import br.com.argus.model.Turma;
+import br.com.argus.model.Secretario;
 import java.util.List;
 
 /**
@@ -19,7 +27,9 @@ import java.util.List;
  */
 public interface IFacade {
     
-    // Usuario
+
+    
+    //  Usuario
     public Usuario buscarLoginUsuario(String login, String senha) throws BussinesException;
     public List<Usuario> buscarTodosUsuarios(String string) throws BussinesException;
     public void inserirOuAtualizar(Usuario usuario) throws BussinesException;
@@ -27,24 +37,91 @@ public interface IFacade {
     public void deletarUsuario (Usuario usuario) throws BussinesException;
     public void buscarUsuario (Usuario usuario) throws BussinesException;
     
-    //Endereco
+    
+    //  Endereco
     public void inserirOuAtualizarEndereco(Endereco endereco) throws BussinesException;
     public void desabilitarEndereco(Endereco endereco) throws BussinesException;
     public void deletarEndereco (Endereco endereco) throws BussinesException;
     public void buscarEndereco (Endereco endereco) throws BussinesException;
     public List<Endereco> buscarTodosEnderecos (Endereco endereco) throws BussinesException;
     
-    //Contato
+    
+    //  Contato
     public void inserirOuAtualizarContato(Contato contato) throws BussinesException;
     public void desabilitarContato(Contato contato) throws BussinesException;
     public void deletarContato (Contato contato) throws BussinesException;
     public void buscarContato (Contato contato) throws BussinesException;
     public List<Contato> buscarTodosContatos (Contato contato) throws BussinesException;
     
-    //Aluno
+    
+    //  Aluno   
     public void inserirOuAtualizarAluno(Aluno aluno) throws BussinesException;
     public void desabilitarAluno(Aluno aluno) throws BussinesException;
     public void deletarAluno (Aluno aluno) throws BussinesException;
     public void buscarAluno (Aluno aluno) throws BussinesException;
     public List<Aluno> buscarTodosAlunos (Aluno aluno) throws BussinesException;
+    
+    
+    //  Cooordenador
+    public void inserirOuAtualizarCoordenador(Coordenador coordenador) throws BussinesException;
+    public void desabilitarCoordenador(Coordenador coordenador) throws BussinesException;
+    public void deletarCoordenador (Coordenador coordenador) throws BussinesException;
+    public void buscarCoordenador(Coordenador coordenador) throws BussinesException;
+    public List<Coordenador> buscarTodosCoordenadores (Coordenador coordenador) throws BussinesException;
+    
+    
+    //  Diretor
+    public void inserirOuAtualizarDiretor(Diretor diretor) throws BussinesException;
+    public void desabilitarDiretor(Diretor diretor) throws BussinesException;
+    public void deletarDiretor(Diretor diretor) throws BussinesException;
+    public void buscarDiretor (Diretor diretor) throws BussinesException;
+    public List<Diretor> buscarTodosDiretores (Diretor diretor) throws BussinesException;
+    
+    
+    //  Disciplina
+      public void inserirOuAtualizarDisciplina(Disciplina disciplina) throws BussinesException;
+    public void desabilitarDisciplina(Disciplina disciplina) throws BussinesException;
+    public void deletarDisciplina (Disciplina disciplina) throws BussinesException;
+    public void buscarDisciplina(Disciplina disciplina) throws BussinesException;
+    public List<Disciplina> buscarTodosDisciplinas (Disciplina disciplina) throws BussinesException;
+    
+    
+    //  Professor
+    public void inserirOuAtualizarProfessor(Professor professor) throws BussinesException;
+    public void desabilitarProfessor(Professor professor) throws BussinesException;
+    public void deletarProfessor(Professor professor) throws BussinesException;
+    public void buscarProfessor(Professor professor) throws BussinesException;
+    public List<Professor> buscarTodosProfessores (Professor professor) throws BussinesException;
+    
+    
+    //  Responsalvel Financeiro
+    public void inserirOuAtualizarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException;
+    public void desabilitarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException;
+    public void deletarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException;
+    public void buscarResp_Fin(Resp_Financeiro resp_Financeiro) throws BussinesException;
+    public List<Resp_Financeiro> buscarTodosResp_FinS(Resp_Financeiro resp_Financeiro) throws BussinesException;
+    
+    
+    //  Secretario    
+    public void inserirOuAtualizarSecretario(Secretario secretario) throws BussinesException;
+    public void desabilitarSecretario (Secretario secretario) throws BussinesException;
+    public void deletarSecretario(Secretario secretario) throws BussinesException;
+    public void buscarSecretario(Secretario secretario) throws BussinesException;
+    public List<Secretario> buscarTodosSecretario (Secretario secretario) throws BussinesException;
+    
+
+    //  SuperUsuario
+    public void inserirOuAtualizarSuperUsuario(SuperUsuario superUsuario) throws BussinesException;
+    public void desabilitarSuperUsuario(SuperUsuario superUsuario) throws BussinesException;
+    public void deletarSuperUsuario(SuperUsuario superUsuario) throws BussinesException;
+    public void buscarSuperUsuario(SuperUsuario superUsuario) throws BussinesException;
+    public List<SuperUsuario> buscarTodosSuperUsuarioS (SuperUsuario superUsuario) throws BussinesException;
+    
+    
+    //  Turma
+    public void inserirOuAtualizarTurma (Turma turma) throws BussinesException;
+    public void desabilitarTurma (Turma turma) throws BussinesException;
+    public void deletarCTurma (Turma turma) throws BussinesException;
+    public void buscarTurma (Turma turma) throws BussinesException;
+    public List<Turma> buscarTodosTurma (Turma turma) throws BussinesException;
 }

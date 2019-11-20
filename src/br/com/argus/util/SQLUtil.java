7f284715fc12c4ec215ed6899043f6c2706c2ec4
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-11-20 08:58:01 -0300

    Criação de Striggers

--- src/br/com/argus/util/SQLUtil.java
+++ src/br/com/argus/util/SQLUtil.java
@@ -13,10 +13,138 @@ package br.com.argus.util;
 public class SQLUtil {
     
     public static final String BUSCAR_LOGIN = "select u from Usuario u where u.login = :login and u.senha = :senha";
+    public static final String BUSCAR_CPF_PROFESSOR = "SELECT u FROM Professor u WHERE u.cpf = :cpf";
+    public static final String BUSCAR_CPF_USUARIO = "SELECT u FROM Usuario u WHERE u.cpf = :cpf";
+    public static final String BUSCAR_CPF_RESPONSAVEL = "SELECT u FROM Resp_Financeiro u WHERE u.cpf = :cpf";
     public static final String BUSCAR_TIPO_USUARIO = "SELECT u FROM Usuario u WHERE u.tipoCargo='";
     public static final String PESQUISA_RES_FINANCEIRO= "SELECT p FROM Resp_Financeiro p WHERE p.nome LIKE '";
     public static String TIPO = "SELECT";
- //  public static final String BUSCAR_TODOS_USUARIOS = "select u from Usuario";
-//    public static final String BUSCAR_ALUNOS_TABLE = "select pf from PessoaFisicaView pf inner join Endereco e on pf.endereco.id = e.id where lower(str(pf.nome)) like :busca or pf.cpf = :busca";
+
+    
+    	
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static final String GATILHOS_LOG = "CREATE OR REPLACE FUNCTION gera_log()\n" + 
+			"RETURNS TRIGGER\n" + 
+			"AS $$\n" + 
+			"BEGIN\n" + 
+			"INSERT INTO log (data, autor, alteracao, tabela)\n" + 
+			"VALUES (now(), user, TG_OP, TG_RELNAME);\n" + 
+			"RETURN NEW;\n" + 
+			"END; \n" + 
+			"$$ LANGUAGE plpgsql;\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_admin\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON administrador\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_aluga\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON aluga\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_caixa\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON caixa\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_contato\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON contato\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_endereco\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON endereco\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_funcionario\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON funcionario\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_pessoa_fisica\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON pessoa_fisica\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_pessoa_juridica\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON pessoa_juridica\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_quarto\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON quarto\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_reserva\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON reserva\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();\n" + 
+			"\n" + 
+			"CREATE TRIGGER tr_gera_log_super_usuario\n" + 
+			"BEFORE INSERT OR UPDATE OR DELETE\n" + 
+			"ON super_usuario\n" + 
+			"FOR EACH ROW\n" + 
+			"EXECUTE PROCEDURE gera_log();";
     
 }
